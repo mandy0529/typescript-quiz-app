294a73dbda64bf2d4897beaf30347dc5e4c9d149
Minji <321963741@naver.com>
2022-01-17 14:41:12 +0900

    memory game app state defined

--- memory-game/src/App.tsx
+++ memory-game/src/App.tsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Wrapper} from './styles/App.styles';
+import {shuffleArray} from './utils/utils';
+import {CardType, createBoard} from './utils/img';
 
 function App() {
-  return <div className="App">app</div>;
+  const [cards, setCards] = useState<CardType[]>(shuffleArray(createBoard()));
+  const [gameWon, setGameWon] = useState(false);
+  const [matchedPairs, setMatchdPairs] = useState(0);
+  const [clickedCard, setClickedCard] = useState<undefined | CardType>(
+    undefined
+  );
+
+  return (
+    <Wrapper className="App">
+      {cards.map((item, index) => {
+        return <p key={index}>{item.id}</p>;
+      })}
+    </Wrapper>
+  );
 }
 
 export default App;
---
+++ memory-game/src/utils/img.tsx
@@ -0,0 +1,42 @@
+import cardback from '../img/card-back.jpeg';
+import lego1 from '../img/lego1.jpeg';
+import lego2 from '../img/lego2.jpeg';
+import lego3 from '../img/lego3.jpeg';
+import lego4 from '../img/lego4.jpeg';
+import lego5 from '../img/lego5.jpeg';
+import lego6 from '../img/lego6.jpeg';
+import lego7 from '../img/lego7.jpeg';
+import lego8 from '../img/lego8.jpeg';
+
+export type CardType = {
+  id: string;
+  flipped: boolean;
+  backImage: string;
+  frontImage: string;
+  clickable: boolean;
+  matchingCardId: string;
+};
+
+const cards: string[] = [
+  lego1,
+  lego2,
+  lego3,
+  lego4,
+  lego5,
+  lego6,
+  lego7,
+  lego8,
+];
+
+export const createBoard = (): CardType[] =>
+  [...cards, ...cards].map((item, index) => ({
+    id: `item${index}`,
+    flipped: false,
+    backImage: cardback,
+    frontImage: item,
+    clickable: true,
+    matchingCardId:
+      index < item.length
+        ? `item${index + cards.length}`
+        : `item${index - cards.length}`,
+  }));
